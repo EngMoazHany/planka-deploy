0bb37dceca78c0c2ab85a92f78877aa58fcdf0e9
Moaz Hany <youremail@example.com>
2025-07-05 09:45:57 +0300

    remove PLANKA branding from login page

--- client/src/locales/it-IT/login.js
+++ client/src/locales/it-IT/login.js
@@ -12,7 +12,7 @@ export default {
       or: 'Oppure',
       pageNotFound_title: 'Pagina non trovata',
       password: 'Password',
-      poweredByPlanka: 'Powered by <1>PLANKA</1>',
+      poweredByPlanka: 'by <1>Moaz El Mahdy © 2025</1>',
       serverConnectionFailed: 'Connesione al server fallita',
       unknownError: 'Errore sconosciuto, prova ancora',
       useSingleSignOn: 'Accedi con SSO',
